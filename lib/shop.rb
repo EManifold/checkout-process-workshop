07ffaf0e0b50f8fddd9c9ee1843f2c109b279d3d
Ellie <elliemmanifold@gmail.com>
2019-12-04 16:20:19 +0000

    Last commit

--- lib/shop.rb
+++ lib/shop.rb
@@ -1,13 +1,22 @@
 class Shop
   def checkout(item)
-    individual_item = item.split('')
-    if individual_item == 'A'
-      50
-    elsif individual_item == 'B'
-      30
-    elsif individual_item == 'C'
-      20
+    individual_items = item.split('')
+    total = 0
+      individual_items.each do |individual_item|
+        if individual_item == 'A' && individual_items.length % 3 != 0
+          total += 50
+        elsif individual_item == 'A' && individual_items.length % 3 == 0 && (individual_items.sort[0] == individual_items.sort[-1])
+          total += 43.333333333333336
+        elsif individual_item == 'B' && individual_items.length % 2 != 0
+          total += 30
+        elsif individual_item == 'B' && individual_items.length % 2 == 0 && (individual_items.sort[0] == individual_items.sort[-1])
+          total += 22.5
+        elsif individual_item == 'C'
+          total += 20
+        elsif individual_item == 'D'
+          total += 15
+        end
     end
-
+  total
   end
 end
--- spec/shop_spec.rb
+++ spec/shop_spec.rb
@@ -17,5 +17,25 @@ describe Shop do
     it "should return 100 when passed in 'AA'" do
       expect(subject.checkout('AA')).to eq 100
     end
+
+    it "should return 15 when passed in 'D'" do
+      expect(subject.checkout('D')).to eq 15
+    end
+
+    it "should return 130 when passed in 'AAA'" do
+      expect(subject.checkout('AAA')).to eq 130
+    end
+
+    it "should return 260 when passed in 'AAAAAA'" do
+      expect(subject.checkout('AAAAAA')).to eq 260
+    end
+
+    it "should return 45 when passed 'BB'" do
+      expect(subject.checkout('BB')).to eq 45
+    end
+
+    it "should return 80 when passed 'AB'" do
+      expect(subject.checkout('AB')).to eq 80
+    end
   end
 end
